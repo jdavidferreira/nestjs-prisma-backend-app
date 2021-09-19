2b5c3a5a852d0941a18ac610a7fe1cd90b2b3ac8
David Ferreira <mrjdavidfg@gmail.com>
2021-09-19 12:58:57 -0500

    Added Prisma client with initial DB schema

---
+++ prisma/migrations/20210919175259_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('TECHNICIAN', 'MANAGER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" VARCHAR NOT NULL,
+    "lasttName" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL,
+    "role" "Role" NOT NULL DEFAULT E'TECHNICIAN',
+    "isLoggedIn" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "address" VARCHAR,
+    "priority" "Priority" NOT NULL DEFAULT E'LOW',
+    "technicianId" INTEGER,
+    "managerId" INTEGER,
+    "executedAt" TIMESTAMP(3),
+    "orderTypeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderType" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "description" TEXT,
+    "color" CHAR(6) NOT NULL,
+
+    CONSTRAINT "OrderType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "OrderType_name_key" ON "OrderType"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "OrderType_color_key" ON "OrderType"("color");
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_technicianId_fkey" FOREIGN KEY ("technicianId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_orderTypeId_fkey" FOREIGN KEY ("orderTypeId") REFERENCES "OrderType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
