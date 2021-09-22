e06d696843249a614cd3dbab144ef464c658e26e
David Ferreira <mrjdavidfg@gmail.com>
2021-09-21 23:36:44 -0500

    Removed App initial controller and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { UserModule } from './user'
 import { PrismaModule } from './user/prisma.module'
 
 @Module({
   imports: [PrismaModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
