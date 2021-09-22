c494e6fe603c63c5633a95a0c006d24b63852355
David Ferreira <mrjdavidfg@gmail.com>
2021-09-21 23:36:09 -0500

    Created global PrismaModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user'
+import { PrismaModule } from './user/prisma.module'
 
 @Module({
-  imports: [UserModule],
+  imports: [PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
