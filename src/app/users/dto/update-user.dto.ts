5584b56c533542c012887bf17628e304336a7faa
David Ferreira <mrjdavidfg@gmail.com>
2022-09-10 18:15:30 -0500

    Renamed User model relationships

--- src/app/users/dto/update-user.dto.ts
+++ src/app/users/dto/update-user.dto.ts
@@ -1,8 +1,4 @@
 import { PartialType } from '@nestjs/swagger'
 import { CreateUserDto } from './create-user.dto'
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  // TODO: find out how to update this relations
-  // technicianOrders?: Array<number>
-  // managerOrders?: Array<number>
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
