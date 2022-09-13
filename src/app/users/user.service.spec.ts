e4f83c7aaea367b0d03ef7aec49c450f474a0639
David Ferreira <mrjdavidfg@gmail.com>
2022-09-12 23:32:45 -0500

    Updated UserService test

--- src/app/users/user.service.spec.ts
+++ src/app/users/user.service.spec.ts
@@ -1,27 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaClient, User, UserRole } from '@prisma/client'
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended'
+
 import { PrismaModule } from 'src/prisma/prisma.module'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateUserDto, UpdateUserDto } from './dto'
 import { UserService } from './user.service'
 
 describe('UserService', () => {
-  let service: UserService
+  let userService: UserService
+  let prisma: DeepMockProxy<PrismaClient>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService],
       imports: [PrismaModule],
-    }).compile()
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile()
 
-    service = module.get<UserService>(UserService)
+    userService = module.get(UserService)
+    prisma = module.get(PrismaService)
   })
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
+    expect(userService).toBeDefined()
   })
 
-  // TODO: set up test db
-  it.skip('returns users', async () => {
-    const result = await service.findAll()
+  describe('findById', () => {
+    it('returns user', () => {
+      prisma.user.findUnique.mockResolvedValueOnce(testUser)
 
-    expect(result).toHaveLength(2)
+      expect(userService.findById(testUser.id)).resolves.toBe(testUser)
+    })
+  })
+
+  describe('findAll', () => {
+    it('returns users', async () => {
+      const testUsers: User[] = [testUser]
+
+      prisma.user.findMany.mockResolvedValueOnce(testUsers)
+
+      const result = await userService.findAll()
+
+      expect(result).toHaveLength(1)
+      expect(result).toBe(testUsers)
+    })
+  })
+
+  describe('create', () => {
+    it('creates a user', () => {
+      const userData: CreateUserDto = {
+        firstName: 'firstNameTest',
+        lastName: 'lastNameTest',
+        email: 'test@email.com',
+        role: UserRole.TECHNICIAN,
+      }
+
+      prisma.user.create.mockResolvedValueOnce(testUser)
+
+      expect(userService.create(userData)).resolves.toBe(testUser)
+    })
+  })
+
+  describe('update', () => {
+    it('updates a user', () => {
+      const userData: UpdateUserDto = {
+        firstName: 'firstNameTest',
+        lastName: 'lastNameTest',
+        email: 'test@email.com',
+        role: UserRole.TECHNICIAN,
+      }
+
+      prisma.user.update.mockResolvedValueOnce(testUser)
+
+      expect(userService.update({ id: testUser.id }, userData)).resolves.toBe(
+        testUser,
+      )
+    })
+  })
+
+  describe('delete', () => {
+    it('deletes a user', () => {
+      prisma.user.delete.mockResolvedValueOnce(testUser)
+
+      expect(userService.delete({ id: testUser.id })).resolves.toBe(testUser)
+    })
   })
 })
+
+const testUser: User = {
+  id: 1,
+  firstName: 'firstNameTest',
+  lastName: 'lastNameTest',
+  email: 'test@email.com',
+  role: UserRole.TECHNICIAN,
+  isLoggedIn: false,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+} as const
