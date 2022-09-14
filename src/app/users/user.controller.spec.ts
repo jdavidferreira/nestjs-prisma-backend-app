022251339a299cef177da93a70e633a33a1f138a
David Ferreira <mrjdavidfg@gmail.com>
2022-09-13 19:30:02 -0500

    Updated UserController test

--- src/app/users/user.controller.spec.ts
+++ src/app/users/user.controller.spec.ts
@@ -1,22 +1,114 @@
+import { NotFoundException } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
+import { User, UserRole } from '@prisma/client'
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended'
+
 import { PrismaModule } from 'src/prisma/prisma.module'
+import { CreateUserDto, UpdateUserDto } from './dto'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 
 describe('UserController', () => {
-  let controller: UserController
+  let userController: UserController
+  let userService: DeepMockProxy<UserService>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
       imports: [PrismaModule],
-    }).compile()
+    })
+      .overrideProvider(UserService)
+      .useValue(mockDeep<UserService>())
+      .compile()
 
-    controller = module.get<UserController>(UserController)
+    userController = module.get(UserController)
+    userService = module.get(UserService)
   })
 
   it('is defined', () => {
-    expect(controller).toBeDefined()
+    expect(userController).toBeDefined()
+  })
+
+  describe('findById', () => {
+    it('returns user', () => {
+      userService.findById.mockResolvedValueOnce(testUser)
+
+      expect(userController.findById(testUser.id)).resolves.toBe(testUser)
+    })
+
+    it('throws NotFound error when user does not exist', () => {
+      const error = new NotFoundException()
+
+      userService.findById.mockRejectedValueOnce(error)
+
+      expect(userController.findById(testUser.id)).rejects.toThrowError(
+        NotFoundException,
+      )
+    })
+  })
+
+  describe('findAll', () => {
+    it('returns users', async () => {
+      const testUsers: User[] = [testUser]
+
+      userService.findAll.mockResolvedValueOnce(testUsers)
+
+      const result = await userController.findAll()
+
+      expect(result).toHaveLength(1)
+      expect(result).toBe(testUsers)
+    })
+  })
+
+  describe('create', () => {
+    it('creates a user', () => {
+      const userData: CreateUserDto = {
+        firstName: 'firstNameTest',
+        lastName: 'lastNameTest',
+        email: 'test@email.com',
+        role: UserRole.TECHNICIAN,
+      }
+
+      userService.create.mockResolvedValueOnce(testUser)
+
+      expect(userController.create(userData)).resolves.toBe(testUser)
+    })
+  })
+
+  describe('update', () => {
+    it('updates a user', () => {
+      const userData: UpdateUserDto = {
+        firstName: 'firstNameTest',
+        lastName: 'lastNameTest',
+        email: 'test@email.com',
+        role: UserRole.TECHNICIAN,
+      }
+
+      userService.update.mockResolvedValueOnce(testUser)
+
+      expect(userController.update(testUser.id, userData)).resolves.toBe(
+        testUser,
+      )
+    })
+  })
+
+  describe('delete', () => {
+    it('deletes a user', () => {
+      userService.delete.mockResolvedValueOnce(testUser)
+
+      expect(userController.delete(testUser.id)).resolves.toBe(testUser)
+    })
   })
 })
+
+const testUser: User = {
+  id: 1,
+  firstName: 'firstNameTest',
+  lastName: 'lastNameTest',
+  email: 'test@email.com',
+  role: UserRole.TECHNICIAN,
+  isLoggedIn: false,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+} as const
